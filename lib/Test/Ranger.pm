95c7e1dfedcbfbb09e5c9e0a422d5190745cf372
Xiong Changnian <xiong@cpan.org>
2010-08-29 03:15:21 -0700

    Fill in OO part of Synopsis and test it.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- lib/Test/Ranger.pm
+++ lib/Test/Ranger.pm
@@ -1,12 +1,68 @@
 package Test::Ranger;
 
-use warnings;
 use strict;
+use warnings;
 use Carp;
 
 use version 0.77; our $VERSION = qv('0.0.1');
 
+use Test::More;
 
+## use
+
+#============================================================================#
+
+######## CLASS METHOD ########
+#
+#	my $obj	= Test::Hump->new();
+#
+#		Returns a hashref blessed into class of calling package
+#
+#		see also: init();
+#
+sub new {
+	my $class	= $_[0];
+	my $self	= {};
+	
+	bless ($self => $class);
+	$self->init(@_);			# init all remaining args
+	
+	return $self;
+}; ## new
+
+######## OBJECT METHOD ########
+#
+#	$obj->init( $AoH );
+#
+#		Initializes $obj with a preconstructed data structure.
+#		$obj is a conventional hash-based object.
+#		See docs for specification.
+#
+sub init {
+	my $self	= $_[0];
+	my $aref	= $_[1];
+	
+	# assign list to hash
+	$self->{-data}	    = $aref;
+	
+	return $self;
+}; ## init
+
+######## OBJECT METHOD ########
+#
+#	$test->execute();
+#
+#		Executes the tests and subtests defined in the object $test.
+#
+sub execute {
+	my $self	= $_[0];
+    
+    pass('DUMMY');
+    done_testing(1);
+    
+    return 1;                           # DUMMY
+    
+}; ## execute
 
 #############################
 ######## END MODULE #########
@@ -15,15 +71,58 @@ __END__
 
 =head1 NAME
 
-Test::Ranger - [One line description of module's purpose here]
+Test::Ranger - Test with data tables, capturing, templates
 
 =head1 VERSION
 
 This document describes Test::Ranger version 0.0.1
 
+NOTE: THIS IS A DUMMY, NONFUNCTIONAL RELEASE.
+
 =head1 SYNOPSIS
 
+    # Object-oriented usage
     use Test::Ranger;
+
+    my $test    = Test::Ranger->new(
+        {
+            -coderef	=> \&Acme::Dummy::hello_mult,
+            -basename	=> 'hello-mult',
+        },
+        
+        {
+            -name       => '4*7',
+            -given      => [ 4, 7 ],
+            -return     => {
+                -is         => 42,
+            },
+            -stdout     => {
+                -like       => [ qw(hello world) ],
+                -matches    => 2,
+                -lines      => 1,
+            },
+        },
+        
+        {
+            -name       => '9*9',
+            -given      => [ 9, 9 ],
+            -return     => {
+                -is         => 81,
+            },
+        },
+        
+        {
+            -name       => 'string',
+            -given      => [ 'monkey' ],
+            -warn       => {
+                -like       => 'dummy',
+            },
+        },
+        
+    ); ## end new
+
+    $test->execute();
+
 
 =for author to fill in:
     Brief code example(s) here showing commonest usage(s).
---
+++ t/all/synopsis-oo.t
@@ -0,0 +1,51 @@
+use strict;
+use warnings;
+ 
+use lib qw{
+	      lib
+	   ../lib
+	../../lib
+};
+
+use Test::Ranger;
+
+# Object-oriented usage
+my $test    = Test::Ranger->new(
+    {
+		-coderef	=> \&Acme::Dummy::hello_mult,
+		-basename	=> 'hello-mult',
+    },
+    
+    {
+        -name       => '4*7',
+        -given      => [ 4, 7 ],
+        -return     => {
+            -is         => 42,
+        },
+        -stdout     => {
+            -like       => [ qw(hello world) ],
+            -matches    => 2,
+            -lines      => 1,
+        },
+    },
+    
+    {
+        -name       => '9*9',
+        -given      => [ 9, 9 ],
+        -return     => {
+            -is         => 81,
+        },
+    },
+    
+    {
+        -name       => 'string',
+        -given      => [ 'monkey' ],
+        -warn       => {
+            -like       => 'dummy',
+        },
+    },
+    
+); ## end new
+
+$test->execute();
+
