21f55ef17c7b2ff046468928101df47c2299e770
Xiong Changnian <xiong@cpan.org>
2010-08-28 20:36:38 -0700

    License: Artistic 2.0
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- lib/Test/Ranger.pm
+++ lib/Test/Ranger.pm
@@ -17,11 +17,9 @@ __END__
 
 Test::Ranger - [One line description of module's purpose here]
 
-
 =head1 VERSION
 
 This document describes Test::Ranger version 0.0.1
-
 
 =head1 SYNOPSIS
 
@@ -31,14 +29,12 @@ This document describes Test::Ranger version 0.0.1
     Brief code example(s) here showing commonest usage(s).
     This section will be as far as many users bother reading
     so make it as educational and exeplary as possible.
-  
-  
+
 =head1 DESCRIPTION
 
 =for author to fill in:
     Write a full description of the module and its features here.
     Use subsections (=head2, =head3) as appropriate.
-
 
 =head1 INTERFACE 
 
@@ -47,7 +43,6 @@ This document describes Test::Ranger version 0.0.1
     interface. These normally consist of either subroutines that may be
     exported, or methods that may be called on objects belonging to the
     classes provided by the module.
-
 
 =head1 DIAGNOSTICS
 
@@ -71,7 +66,6 @@ This document describes Test::Ranger version 0.0.1
 
 =back
 
-
 =head1 CONFIGURATION AND ENVIRONMENT
 
 =for author to fill in:
@@ -83,7 +77,6 @@ This document describes Test::Ranger version 0.0.1
   
 Test::Ranger requires no configuration files or environment variables.
 
-
 =head1 DEPENDENCIES
 
 =for author to fill in:
@@ -93,7 +86,6 @@ Test::Ranger requires no configuration files or environment variables.
     module's distribution, or must be installed separately. ]
 
 None.
-
 
 =head1 INCOMPATIBILITIES
 
@@ -106,7 +98,6 @@ None.
 
 None reported.
 
-
 =head1 BUGS AND LIMITATIONS
 
 =for author to fill in:
@@ -118,45 +109,21 @@ None reported.
     limitations on the size of data sets, special cases that are not
     (yet) handled, etc.
 
-No bugs have been reported.
 
 Please report any bugs or feature requests to
 C<bug-test-ranger@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org>.
 
-
 =head1 AUTHOR
 
-Xiong Changnian  C<< <xiong@sf-id.com> >>
+Xiong Changnian  C<< <xiong@cpan.org> >>
 
+=head1 LICENSE
 
-=head1 LICENCE AND COPYRIGHT
+Copyright (C) 2010 Xiong Changnian C<< <xiong@cpan.org> >>
 
-Copyright (c) 2010, Xiong Changnian C<< <xiong@sf-id.com> >>. All rights reserved.
+This library and its contents are released under Artistic License 2.0:
 
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
+L<http://www.opensource.org/licenses/artistic-license-2.0.php>
 
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+=cut
