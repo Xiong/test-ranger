33099d45a64ae4c7f6041ce92a2a8c0bcc4c1551
Xiong Changnian <xiong@cpan.org>
2010-11-28 10:09:09 -0800

    Create trish.pl and Shell.pm
    * trish.pl -- Script to invoke Shell.pm
    * Shell.pm -- Interactive shell, with test script tagging.
    
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ bin/trish.pl
@@ -0,0 +1,27 @@
+#!/run/bin/perl
+#       trish.pl
+#       = Copyright 2010 Xiong Changnian <xiong@cpan.org> =
+#       = Free Software = Artistic License 2.0 = NO WARRANTY =
+
+use strict;
+use warnings;
+
+use File::Spec::Functions qw(
+    catdir
+    catfile
+    catpath
+    splitpath
+    curdir
+    rootdir
+    updir
+    canonpath
+);
+use Cwd;
+use Devel::Comments '###', '####';
+
+#
+
+
+__END__
+
+
