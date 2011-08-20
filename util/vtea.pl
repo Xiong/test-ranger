3532cb3cd83a9d2c05897d27e998b6a067139cac
Xiong Changnian <xiong@cpan.org>
2011-08-20 02:30:07 -0700

    Some demo scripts.
    * hello-gtk.pl is from
    	http://gtk2-perl.sourceforge.net/doc/gtk2-perl-study-guide/c403.html
    
    * vtea.pl is a modification-sandbox of vte.pl

---
+++ util/vtea.pl
@@ -0,0 +1,45 @@
+#!/run/bin/perl
+
+    use strict;
+    use Glib qw(TRUE FALSE);
+    use Gtk2 -init;
+    use Gnome2::Vte;
+    use Devel::Comments '###';
+    
+    # create things
+    my $window = Gtk2::Window->new;
+    my $scrollbar = Gtk2::VScrollbar->new;
+    my $hbox = Gtk2::HBox->new;
+    my $terminal = Gnome2::Vte::Terminal->new;
+    
+    # set up scrolling
+    $scrollbar->set_adjustment ($terminal->get_adjustment);
+    
+    # lay 'em out
+    $window->add ($hbox);
+    $hbox->pack_start ($terminal, TRUE, TRUE, 0);
+    $hbox->pack_start ($scrollbar, FALSE, FALSE, 0);
+    $window->show_all;
+    
+    # hook 'em up
+    $terminal->fork_command ('/bin/bash', ['bash', '-login'], undef,
+                           '/tmp', FALSE, FALSE, FALSE);
+    $terminal->signal_connect (child_exited => sub { Gtk2->main_quit });
+    $window->signal_connect (delete_event =>
+                           sub { Gtk2->main_quit; FALSE });
+    
+
+#~ my %style   = $terminal->style;
+#~ ### %style
+
+#~ my @lines   ;
+#~ foreach (sort keys %style) {push @lines, "$_: $style{$_}"};
+#~ 
+#~ my $data    = join qq{\n}, @lines, qq{\n};
+#~ 
+#~ $terminal->feed ($data);
+    
+    # turn 'em loose
+    Gtk2->main;
+
+__END__
